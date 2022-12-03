289e2d6042a425f33b449334604e80087fadbdcf
chrisjn123 <chrisjnickell@gmail.com>
2022-12-02 22:37:15 -0700

    Part 2 solution and test file

--- day2/day2.py
+++ day2/day2.py
@@ -1,2 +1,55 @@
+#with open('test.txt', 'r') as fh:
 with open('input.txt', 'r') as fh:
-    data = fh.readlines()
+    lines = fh.readlines()
+    L = [line.split()[0] for line in lines]
+    R = [line.split()[1] for line in lines]
+
+guide = {
+    "A": "Rock",
+    "B": "Paper",
+    "C": "Scissors",
+    "X": "Rock",
+    "Y": "Paper",
+    "Z": "Scissors" 
+}
+beats = {
+    "Rock": "Scissors",
+    "Scissors": "Paper",
+    "Paper": "Rock"
+}
+
+score = {
+    "Rock": 1,
+    "Paper": 2,
+    "Scissors": 3
+}
+
+scoreA = 0
+
+for l, r in zip(L, R):
+    opp = guide[l]
+    if r == "X":
+        me = beats[guide[l]]
+    elif r == 'Y':
+        me = opp
+    else:
+        if opp == 'Scissors':
+            me = "Rock"
+        elif opp == 'Rock':
+            me = 'Paper'
+        else:
+            me = "Scissors"
+
+    scoreA += score[me]
+    if opp == me:
+        scoreA += 3
+    elif opp == "Rock" and me == "Paper":
+        scoreA += 6
+    elif opp == "Paper" and me == "Scissors":
+        scoreA += 6
+    elif opp == "Scissors" and me == "Rock":
+        scoreA += 6
+    else:
+        pass
+    #print(f'\t{scoreA}')
+print(scoreA)
