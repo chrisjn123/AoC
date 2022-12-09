a80e66ad07e00578cc60762b88cbc4b5d0cfa214
Chris Nickell <chrisjnickell@gmail.com>
2022-12-09 05:56:40 -0700

    Part 2

--- day9/day9.py
+++ day9/day9.py
@@ -7,6 +7,7 @@ else:
     data = [line.strip() for line in open('input.txt').readlines()]
 
 # day 9
+'''
 visited = 0
 tracked = defaultdict(int)
 
@@ -63,10 +64,6 @@ def move(direction: str, amount: int):
         
                 visited += 1
                 tracked[(t_loc['x'], t_loc['y'])] += 1
-    #print(f'H: {h_loc["x"]}, {h_loc["y"]}')
-    #print(f'T: {t_loc["x"]}, {t_loc["y"]}')
-    #print('------------------')
-
 
 for line in data:
     move(
@@ -74,3 +71,65 @@ for line in data:
         int(line.split()[-1])
     )
 print(f'Visited: {len(tracked)}')
+'''
+
+# Part 2
+
+def generic_move(direction:str, position:int) -> tuple:
+    if direction == 'R':
+        tmp = (position[0] + 1, position[1])
+    elif direction == 'L':
+        tmp = (position[0] - 1, position[1])
+    elif direction == 'D':
+        tmp = (position[0], position[1] - 1)
+    else:
+        tmp = (position[0], position[1] + 1)
+    return tmp
+
+def needs_to_move(h_x, h_y, t_x, t_y) -> bool:
+    return abs(t_x - h_x) > 1 or abs(t_y - h_y) > 1
+
+def array_move_tail(current_head, target_tail) -> tuple:
+    t_x, t_y = target_tail
+    h_x, h_y = current_head
+    next_tail = target_tail
+
+    if needs_to_move(h_x, h_y, t_x, t_y):
+        if h_x < t_x:
+            next_tail = generic_move('L', next_tail)
+        else:
+            next_tail = generic_move('L', next_tail)
+
+        if h_y < t_y:
+            next_tail = generic_move('D', next_tail)
+        else:
+            next_tail = generic_move('U', next_tail)
+        
+        return next_tail
+    else:
+        return target_tail  
+    
+
+def run(moves: list, num_knots=1):
+    s = (0,0)
+    tracked_points = set([s])
+
+    head = s
+    tail = [s for _ in range(num_knots)]
+
+    for move in moves:
+        direction, quant = move.split()
+
+        for _ in range(int(quant)):
+            head = generic_move(direction, head)
+
+            in_line = head
+            for i, tl in enumerate(tail):
+                tail[i] = array_move_tail(in_line, tl)
+                in_line = tail[i]
+            
+            tracked_points.add(tail[-1])
+    
+    return len(tracked_points)
+
+print(run(data, 9))
