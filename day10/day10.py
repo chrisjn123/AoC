f1af57df522230a1cbbd44eaa5f31c21937bb648
Chris Nickell <chrisjnickell@gmail.com>
2022-12-09 23:15:37 -0700

    Part 2

--- day10/day10.py
+++ day10/day10.py
@@ -6,9 +6,29 @@ cycles = 1
 
 c2v = defaultdict(int)
 c2v[0] = 1
+crt = ['.' for i in range(240)]
+idx = 0
+prev = X
+
+def draw():
+    global crt, X, cycles, idx
+    print(f'Cycle [{cycles}]: Drawing pixel [{cycles-1}]', end=' ')
+    if X != prev:
+        idx = 0
+    
+    sprite = [X - 1, X, X+1]
+    if ((cycles-1)  % 40) in sprite:
+        crt[cycles - 1] = "#"
+    idx += 1
+    if idx > 2:
+        idx = 0
+    print(crt[cycles - 1])
+    
+
 for ins in data:
+    draw()
     if ins == 'noop':
-        c2v[cycles] = X 
+        c2v[cycles] = X
         cycles += 1
     else:
         val = int(ins.split()[-1])
@@ -18,28 +38,22 @@ for ins in data:
         
         # cycle 2
         c2v[cycles] = X
+        draw()
         cycles += 1
+        prev = X
         X = X + val
         c2v[cycles] = X
+        draw()
     
-    
+rows = [crt[i:i+40] for i in range(len(crt), 40) ]
 
 s = 0
 for i in [20, 60, 100, 140, 180, 220]:
     s += (c2v[i] * i)
     print(c2v[i] * i)
 print(s)
-'''
-c2i = defaultdict(str)
-cycles = 1
-for line in data:
-    if 'noop' == line:
-        c2i[cycles] = line
-        cycles += 1
-    else:
-        c2i[cycles] = line
-        cycles += 1
-        c2[]
+print()
+print()
 
-
-        '''
+for i in range(0, 240, 40):
+    print(''.join(crt[i:i+40]))
