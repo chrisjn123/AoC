035083edc6a30a5d02c66838a9d339040804b8ab
Chris Nickell <chrisjnickell@gmail.com>
2022-12-14 00:38:16 -0700

    Both parts

--- day14/day14.py
+++ day14/day14.py
@@ -1,15 +1,24 @@
-from functools import cmp_to_key
 from time import perf_counter
-from copy import copy, deepcopy
 from collections import defaultdict
 from colorama import Fore
 
 
-def void(x, y, grid) -> bool:
+def blocked(sands) -> bool:
+    ret = False
+    if (499, 1) in sands and (500, 1) and (501, 1) in sands:
+        ret = True
+
+    return ret
+
+def void(x, y, grid, sands) -> bool:
     column = {key: value 
         for key, value in grid.items() 
-        if key[0] == x and y < key[1]}
-    if '#' in column.values():
+        if key[0] == x and y < key[1] }
+    for sand in sands:
+        xS, yS = sand
+        if xS == x and yS > y:
+            column[sand] = 'o'
+    if '#' in column.values() or 'o' in column.values():
         return False
     return True
     
@@ -17,13 +26,13 @@ def void(x, y, grid) -> bool:
 def main() -> None:
     with open('input.txt', 'r') as fh:
         data = [line for line in fh.readlines()]
-
     
-    sand_at_rest = 0
     grid = defaultdict(str)
     grid[(500, 0)] = '+'
     for line in data:
-        rock_lines = [(int(pair.split(',')[0]), int(pair.split(',')[-1])) for pair in line.split('->')]
+        rock_lines = [(int(pair.split(',')[0]),
+            int(pair.split(',')[-1])) 
+            for pair in line.split('->')]
         
         for i in range(len(rock_lines) -1):
             x1, y1 = rock_lines[i]
@@ -31,7 +40,7 @@ def main() -> None:
 
             if x1 == x2:
                 if y1 > y2:
-                    y1, x2 = y2, y1
+                    y1, y2 = y2, y1
                 for y in range(y1, y2 + 1):
                     grid[(x1, y)] = '#'
             elif y1 == y2:
@@ -39,6 +48,8 @@ def main() -> None:
                     x1, x2 = x2, x1
                 for x in range(x1, x2 + 1):
                     grid[(x, y1)] = '#'
+            else:
+                print(rock_lines[i])
 
     
     # Process sand
@@ -51,14 +62,31 @@ def main() -> None:
     min_y = 0
     max_y = max([pair[-1] for pair in grid.keys()]) + 2
 
+    for x in range(min_x -100000, max_x + 100000 + 1):
+        grid[(x,max_y)] = '#'
+
+    '''for y in range(min_y, max_y + 1):
+        for x in range(min_x, max_x + 1):
+            pair = (x, y)
+            if grid[pair] != '#' and grid[pair] != '+':
+                if pair in sands:
+                    print('o', end='')
+                else:
+                    print('.', end='')
+            else:
+                print(grid[pair], end='')
+        print()
+    count = input('continue? ')
+    if count == 'n':
+        return'''
 
     while True:
         #print(f'There are [{len(sands) - 1}] sand particles at rest.')
         x, y = sands[idx]
         # if cell below me is available (i.e. not sand or rock)
-        if  grid[(x, y+1)] != '#' and (x, y+1) not in sands:
+        if grid[(x, y+1)] != '#' and (x, y+1) not in sands:
+            isVoid = blocked(sands)
             sands[idx] = (x, y+1)
-            isVoid = void(x, y, grid)
         # elif, diag left is available
         elif grid[(x-1, y+1)] != '#' and (x-1, y+1) not in sands:
             sands[idx] = (x-1, y+1)
@@ -70,28 +98,30 @@ def main() -> None:
             print(f'There are [{len(sands)}] sand particles at rest.')
             idx += 1
             sands.append((500, 0))
+            if blocked(sands):
+                print(f'Blocked State Reached: {len(sands)}')
+                break
         # if void found, break
         if isVoid:
             sands.pop(-1)
-            print(f'Void Reached: {len(sands)}')
+            print(f'Blocked State Reached: {len(sands)}')
             break
 
 
-        if len(sands) -1 == 627 and False:
-            for y in range(min_y, max_y + 1):
-                for x in range(min_x, max_x + 1):
-                    pair = (x, y)
-                    if grid[pair] != '#' and grid[pair] != '+':
-                        if pair in sands:
-                            print(Fore.GREEN + 'o' + Fore.RESET, end='')
-                        else:
-                            print('.', end='')
+        '''for y in range(min_y, max_y + 1):
+            for x in range(min_x, max_x + 1):
+                pair = (x, y)
+                if grid[pair] != '#' and grid[pair] != '+':
+                    if pair in sands:
+                        print(Fore.GREEN + 'o' + Fore.RESET, end='')
                     else:
-                        print(Fore.YELLOW + grid[pair] + Fore.RESET, end='')
-                print()
-            count = input('continue? ')
-            if count == 'n':
-                return
+                        print('.', end='')
+                else:
+                    print(Fore.YELLOW + grid[pair] + Fore.RESET, end='')
+            print()
+        #count = input('continue? ')
+        if count == 'n':
+            return'''
 
 
 if __name__ == "__main__":
