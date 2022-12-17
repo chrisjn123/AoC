fbf9a81410433ef0c5f572abd9cfde335e950a80
Chris Nickell <chrisjnickell@gmail.com>
2022-12-17 12:20:36 -0700

    Refactor

--- day17/day17.py
+++ day17/day17.py
@@ -1,144 +1,97 @@
 from collections import defaultdict
-with open('test.txt', 'r') as fh:
-    jet = [ch for ch in fh.readlines()[0]]
+from itertools import cycle
+import os
+import  copy
 
-rocks = []
-rocks.append([['#','#', '#', '#']])
-rocks.append([
-    ['', '#',''],
-    ['#', '#','#'],
-    ['', '#', '']
-])
-rocks.append([
-    ['', '', '#'],
-    ['', '',  '#'],
-    ['#', '#', '#']
-])
-rocks.append([
-    ['#'],
-    ['#'],
-    ['#'],
-    ['#']
-])
-rocks.append([
-    ['#', '#'],
-    ['#', '#'],
-])
-rock_occ = set()
-
-def addToSet(stopped):
-    lx, ly, lRock = stopped
-    for i in range(len(lRock)):
-        for j in range(len(lRock[0])):
-            if lRock[i][j] == '#':
-                rock_occ.add((j + lx, i + ly))
-def checkIntersectionX(rx, ry, rock, direction='left') -> bool:
-    global rock_occ
-
-    dx = -1 if direction.lower() == 'left' else 1
-
-    fallingCoords = set()
-    fx, fy, fRock = rx, ry, rock
-    for i in range(len(fRock)):
-        for j in range(len(fRock[0])):
-            if fRock[i][j] == '#':
-                fallingCoords.add((j + fx, i + fy))
-
-    for x,y in fallingCoords:
-        if (x+dx, y) in rock_occ:
-            return True
-    return False
-
-def checkIntersection(falling) -> bool:
-    global rock_occ
-    #if len(list(stopped_rocks.keys())) < 1:
-    #    return True        
-    
-    fallingCoords = set()
-    fx, fy, fRock = falling
-    for i in range(len(fRock)):
-        for j in range(len(fRock[0])):
-            if fRock[i][j] == '#':
-                fallingCoords.add((j + fx, i + fy))
-
-    for x,y in fallingCoords:
-        if (x, y-1) in rock_occ:
-            return True
-    return False
-
-
-
-width = 7
-height = 3
-rock_idx = 0
-stopped_rocks = defaultdict()
-stop_id = 1
-rock_stop_limit = 2022
-y_limit = 0
-lastX, lastY = 0,0
-need_new_rock = True
-lastRock = None
-
-def visualize(x, y, rType):
-    coord = set()
-    for i in range(len(rType)):
-        for j in range(len(rType[0])):
-            if rType[i][j] == '#':
-                coord.add((j + x, i + y))
-    yMax = [y for _, y in rock_occ]
-    if len(yMax) == 0:
-        yMax = [0]
-    print('---------------')
-    for iy in range(0, max(yMax) + 10):
-        for ix in range(0, 7):
-            if (ix, iy) in coord:
+HEIGHT = 3
+COMPLETED_POINTS = set()
+stopped = 0
+def printRock(rock):
+    val = int(max([i.imag for i in rock]))
+    for y in range(1 + val, -1, -1):
+        for x in range(0, 7):
+            if complex(x, y) in rock:
                 print('@', end='')
-            elif (ix, iy) in rock_occ:
+            elif complex(x, y) in COMPLETED_POINTS:
                 print('#', end='')
             else:
                 print('.', end='')
         print()
-    
-while jet:
-    # jet current jet motion, circularly add to jets list
-    jet_now = jet.pop(0)
-    jet.append(jet_now)
-    
-    # if starting new OR I have a stopped rock
-    if need_new_rock:
-        if lastRock is not None:
-            rock_pos = (2 , height + lastRock[1] + y_limit)  
-        else:
-            rock_pos = (2 , height)  
-        rock = rocks[rock_idx]                              # Rock type
-        rock_idx += 1                                       # set for next stoppage
-        if rock_idx > 4:
-            rock_idx = 0
-        need_new_rock = False                               # kill new rock init
-        rx, ry = rock_pos                                   # extract position
-    
-    #visualize(rx, ry, rock)
-    
-    if checkIntersection((rx, ry, rock)) or ry == 0:
-        stopped_rocks[stop_id] = (rx, ry, rock)
-        need_new_rock = True
-        y_limit = y_limit + len(rock) 
-        lastRock = (rx, ry, rock)
-        addToSet(lastRock)
-        print(f'Stop count = {stop_id}\tY: {max(y for _, y in rock_occ)}')
-        stop_id += 1
+
+def can_move_x(rock, dx) -> bool:
+    ret = True
+    for coord in rock:
+        if coord - 1 in COMPLETED_POINTS:
+            ret = False
+            break
+    min_x = int(min([x.real for x in rock]))
+    max_x = int(max([x.real for x in rock]))
+    if min_x == 0 or max_x >= 6:
+        ret = False
+    return ret
+
+def can_move_y(rock) -> bool:
+    ret = True
+    if int(min([a.imag for a in rock])) == 0:
+        ret =  False
     else:
-        ry -= 1
-        match jet_now:
-            case '<':
-                if rx > 0 and not checkIntersectionX(rx, ry, rock):
-                    rx -= 1
-            case '>':
-                if rx + len(rock[0])  < width and not checkIntersectionX(rx, ry, rock, direction='right'):
-                    rx += 1
-    #visualize(rx, ry, rock)
-    if stop_id >= 2022:
-        break
+        for coord in rock:
+            if (coord - 1j) in COMPLETED_POINTS:
+                ret = False
+                break
+    return ret 
 
-print(f'{max(y for _, y in rock_occ)}')
+def  update_height_of_rock(rock):
+    for i, coord in enumerate(rock):
+        coord += complex(0, HEIGHT)
+        rock[i] = coord
+    return rock
 
+def main():
+    global HEIGHT, stopped
+    with open('test.txt', 'r') as fh:
+        jet = [ch for ch in fh.readlines()[0]]
+    jet = cycle([1 if ch == '>' else -1 for ch in jet])
+
+    rocks = cycle([
+        [2+0j, 3+0j, 4+0j, 5+0j], # line horizontal
+        [3+0j, 2 + 1j, 3+1j, 4+1j, 3+2j], # plus
+        [4+0j, 4+1j, 2+2j, 3+2j, 4+2j], # L shape (reversed)
+        [2+0j, 2+1j, 2+2j, 2+3j], # line vertical
+        [2+0j, 3+0j, 2+1j, 3+1j], # square
+    ])
+
+    rock = copy.copy(next(rocks))
+    rock = update_height_of_rock(rock)
+
+    # main loop
+    while stopped < 2022:
+        x_dir = next(jet)
+        
+        # if can move left or right
+        if can_move_x(rock, x_dir) and can_move_y(rock):
+            for i, coord in enumerate(rock):
+                rock[i] += x_dir
+        # if it can move down
+        if can_move_y(rock):
+            for i, _ in enumerate(rock):
+                rock[i] -= 1j
+        # otherwise its done, so generate new rock
+        else:
+            for coord in rock:
+                COMPLETED_POINTS.add(coord)
+            HEIGHT = int(max([a.imag for a in COMPLETED_POINTS])) + 3
+            rock = copy.copy(next(rocks))
+            rock = update_height_of_rock(rock)
+            stopped += 1
+        '''os.system('cls')
+        printRock(rock)
+        print('='*7)
+        print()'''
+    print()
+
+
+    
+
+if __name__ == '__main__':
+    main()
